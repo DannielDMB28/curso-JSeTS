9809fcbe8bd1576966f3e7b3233a31b0524294e6
DannielDMB28 <danniel.moreiira04@hotmail.com>
2025-06-17 19:41:29 -0300

    curso de JavaScript

---
+++ JS/aula10(tipos-de-dados-primitivos)/index.js
@@ -0,0 +1,14 @@
+const nome = 'Daniel'; // string
+const nome1 = "Daniel"; // string
+const nome2 = `Daniel`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta para o local nenhum na memória
+const sobrenomeAluano = null; // Nulo -> não aponta para local nenhum na memória
+const aprovado = false; // Boolean -> true, false 
+
+console.log(typeof nome, nome) // typeof -> serve para identificar o tipo da variável
+console.log(typeof num1, num1)
+console.log(typeof nomeAluno, nomeAluno)
+console.log(typeof sobrenomeAluano, sobrenomeAluano)
+console.log(typeof aprovado, aprovado)
